18d884c713dec14d1e9907a3de20401a0011754b
twmrsmrsm <twmrsmrsm.work@yandex.ru>
2018-10-19 15:23:02 +0300

    2test19102018

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <tgmath.h>
 
- int poo () {
+float poo (float m, float t, float x, float RES){
+    RES = (1/(t*sqrt(2*3.140)))*exp(-pow((x-m),2)/2*(pow(t,2)));
+    return RES;
 }
- int foo () {
-    std::cout<< poo << '\n';
-    std::cout<< poo << '\n';
-    std::cout<< poo << '\n';
-}
-int main (){
-    std::cout<< foo << '\n';
+int main() {
+    float m=1, t=2, x=3, RES;
+    std::cout << poo <<std::endl;
     return 0;
+
 }
